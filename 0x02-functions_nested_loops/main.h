315e726b421b9cd71b671c5b6eb50172a79d9753
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-15 20:11:14 -0700

    Check for alphabetic character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 int _putchar(char c); /* _putchar prototype */
 void print_alphabet(void); /* print_alphabet prototype */
 void print_alphabet_x10(void); /* print_alphabet_x10 prototype */
-int _islower(int c); /* _isalpha prototype */
+int _islower(int c); /* _islower prototype */
+int _isalpha(int c); /* _isalpha prototype */
