077a3938c40fbed1f06bdf0bdb4555a169af7e2b
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-15 21:05:29 -0700

    Compute the absolute value of an integer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,4 @@ void print_alphabet_x10(void); /* print_alphabet_x10 prototype */
 int _islower(int c); /* _islower prototype */
 int _isalpha(int c); /* _isalpha prototype */
 int print_sign(int n); /* print_sign prototype */
+int _abs(int n); /*_abs prototype */
