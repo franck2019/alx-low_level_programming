066263f54cdb665141496e390731186b65b3dd77
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-16 03:38:26 -0700

    Print the last digit of a number

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,8 @@ int _islower(int c); /* _islower prototype */
 int _isalpha(int c); /* _isalpha prototype */
 int print_sign(int n); /* print_sign prototype */
 int _abs(int n); /*_abs prototype */
+int print_last_digit(int); /*print_last_digit prototype */
+void jack_bauer(void); /* jack_bauer prototype */
+void times_table(void); /* times_table prototype */
+int add(int, int); /* add prototype */
+void print_to_98(int n); /* print_to_98 prototype */
