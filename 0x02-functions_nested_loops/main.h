45b10d62ad17f460dc74f3ec98fee2b11eff6754
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-15 20:40:30 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ void print_alphabet(void); /* print_alphabet prototype */
 void print_alphabet_x10(void); /* print_alphabet_x10 prototype */
 int _islower(int c); /* _islower prototype */
 int _isalpha(int c); /* _isalpha prototype */
+int print_sign(int n); /* print_sign prototype */
