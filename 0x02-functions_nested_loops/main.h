f3aff7c4e2240b217c1659f1c96a85a0262a911d
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-16 13:28:01 -0700

    Avoid double including

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,28 @@
-#include <stdio.h>
-int _putchar(char c); /* _putchar prototype */
-void print_alphabet(void); /* print_alphabet prototype */
-void print_alphabet_x10(void); /* print_alphabet_x10 prototype */
-int _islower(int c); /* _islower prototype */
-int _isalpha(int c); /* _isalpha prototype */
-int print_sign(int n); /* print_sign prototype */
-int _abs(int n); /*_abs prototype */
-int print_last_digit(int); /*print_last_digit prototype */
-void jack_bauer(void); /* jack_bauer prototype */
-void times_table(void); /* times_table prototype */
-int add(int, int); /* add prototype */
-void print_to_98(int n); /* print_to_98 prototype */
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar prototype */
+int _putchar(char);
+/* print_alphabet prototype */
+void print_alphabet(void);
+/* print_alphabet_x10 prototype */
+void print_alphabet_x10(void);
+/* _islower prototype */
+int _islower(int c);
+/* _isalpha prototype */
+int _isalpha(int c);
+/* print_sign prototype */
+int print_sign(int n);
+/*_abs prototype */
+int _abs(int n);
+/* print_last_digit prototype */
+int print_last_digit(int);
+/* jack_bauer prototype */
+void jack_bauer(void);
+/* times_table prototype */
+void times_table(void);
+/* add prototype */
+int add(int, int);
+/* print_to_98 prototype */
+void print_to_98(int n);
+#endif
