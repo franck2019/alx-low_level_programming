84cc66bbfecf0f7c54f884abcc27c44888ff9a83
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-15 19:55:01 -0700

    Check for lowercase character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 int _putchar(char c); /* _putchar prototype */
 void print_alphabet(void); /* print_alphabet prototype */
 void print_alphabet_x10(void); /* print_alphabet_x10 prototype */
+int _islower(int c); /* _isalpha prototype */
