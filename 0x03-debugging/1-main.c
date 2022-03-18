dd2d197b19165afe1de837ece57842eb008774af
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-17 22:57:23 -0700

    Add task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	i = 0;	
+	i = 0;
 	while (i < 10)
 	{
 		/* putchar(i);*/ break;
 	}
-	printf("Infinite loop avoided! \\o/\n");	
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
