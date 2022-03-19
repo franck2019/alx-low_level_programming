0a6694080a92d647ac945742fcc61023dfc17f67
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-19 15:22:23 -0700

    function multiplying 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 
 /**
- * _isdigit - function that checks for digit(0 through 9).
- * @c: character as an integer
+ * mul - function that multiplies two integers.
+ * @a: an integer
+ * @b: an integer
  *
- * Return: 1 if c is a digit, otherwise 0
+ * Return: a * b
  */
 int mul(int a, int b)
 {
