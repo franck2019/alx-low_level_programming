e3d683a6ec65aff4d62b9cec71da2e16fbb6da07
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-22 08:58:33 -0700

    Add task-2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: a pointer of char
+ *
+ * Return: return the length of the string.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ int _putchar(char c);
 void reset_to_98(int *n);
 /* swap_int prototype */
 void swap_int(int *a, int *b);
+/*_strlen prototype */
+int _strlen(char *s);
 #endif /* MAIN_H */
