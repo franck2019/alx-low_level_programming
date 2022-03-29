b5de8b3b75aee32652537b306e9627528ef23a5b
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-28 17:51:31 -0700

    searche a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 /* _strspn prototype */
 unsigned int _strspn(char *s, char *accept);
+/* _strpbrk prototype */
+char *_strpbrk(char *s, char *accept);
 #endif /* MAIN_H */
