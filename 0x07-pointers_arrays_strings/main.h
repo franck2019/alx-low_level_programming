708805762bcef01c8f100919f0ea97867d6fab97
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-30 13:07:13 -0700

    locate a substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,4 +15,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 /* _strpbrk prototype */
 char *_strpbrk(char *s, char *accept);
+/* _strstr prototype */
+char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */
