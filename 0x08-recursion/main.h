0a673de4efe2d3fd0e55f4fa0f83c4aa3e374281
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-31 18:07:04 -0700

    returns the length of a string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,6 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 /* _print_rev_recursion */
 void _print_rev_recursion(char *s);
+/* _strlen_recursion prototype */
+int _strlen_recursion(char *s);
 #endif /* MAIN_H */
