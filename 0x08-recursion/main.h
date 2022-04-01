0f8f2cbba673d96c91cc94ee66f2d1c846636240
Franck Tchamo <francktchamo@yahoo.fr>
2022-03-31 17:23:40 -0700

    print a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* _putchar prototype */
+int _putchar(char c);
+/* _puts_recursion prototype */
+void _puts_recursion(char *s);
+#endif /* MAIN_H */
