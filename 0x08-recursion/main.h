861dfc79e29522b34f362bb1ba036652dccd26b7
Franck Tchamo <francktchamo@yahoo.fr>
2022-04-01 06:39:49 -0700

    Add wildcmp function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -21,4 +21,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 /* is_palindrome prototype */
 int is_palindrome(char *s);
+/* wildcmp prototype */
+int wildcmp(char *s1, char *s2);
 #endif /* MAIN_H */
