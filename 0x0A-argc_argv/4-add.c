c78c702926fe8ba28fa367d1690c9ed71a214261
Franck Tchamo <francktchamo@yahoo.fr>
2022-04-10 03:36:31 -0700

    Add positive numbers(command  line arguments)

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: number of arguments
+ * @argv: pointer of pointer of charrepresenting arguments.
+ *
+ * Return: nothing(EXIT_SUCCESS)
+ */
+int main(int argc, char **argv)
+{
+	int i, j, sum, len;
+	char arg;
+
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			len = strlen(argv[i]);
+			for (j = 0; j < len; j++)
+			{
+				arg = argv[i][j];
+				if (!isdigit(arg))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);	
+	}
+	return (EXIT_SUCCESS);
+}
