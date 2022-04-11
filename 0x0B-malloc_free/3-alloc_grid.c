b69dcd120e983e3c8f911c4b9b7a4b4fef807818
Franck Tchamo <francktchamo@yahoo.fr>
2022-04-11 03:29:27 -0700

    Allocate pointer to a 2 dimensional array of integers with malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer to a 2 dimensional
+ * array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: a 2 dimensional array of integers, otherwise NULL.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **myGrid = (int **) NULL;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	myGrid = (int **) malloc(height * sizeof(int *));
+	if (myGrid != NULL)
+	{
+		for (i = 0; i < height; i++)
+			myGrid[i] = (int *) malloc(width*sizeof(int));
+		for (i = 0; i < height; i++)
+			for (j = 0; j < width; j++)
+				myGrid[i][j] = 0;
+	}
+	return (myGrid);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -11,4 +11,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 /* str_concat prototype */
 char *str_concat(char *s1, char *s2);
+/* alloc_grid prototype */
+int **alloc_grid(int width, int height);
 #endif /* HOLBERTON_H */
