6e05cfe37fd9b13c0cff502ba715d44950b8b97d
Franck Tchamo <francktchamo@yahoo.fr>
2022-04-21 22:31:47 -0700

    Add 101-hello_holberton.asm script

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+; -----------------------------------------------------------------------------------------
+; Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
+;
+;  nasm -f elf64 101-hello_holberton.asm && 
+;  gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
+;----------------------------------------------------------------------------------------
+
+	global	main
+	extern	puts
+
+	section .text
+main:					; This is called by Clibrary startup code
+	mov	rdi, message		; First integer (or pointer) argument in rdi
+	call	puts			; puts(message)
+	ret				; Return from main back into C library
+
+message:
+	db 	"Hello, Holberton",0    ; Note strings must be terminated with 0 in C
