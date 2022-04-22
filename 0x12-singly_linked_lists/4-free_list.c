df1d8839da40fec30390673b4c749acc60c3c895
Franck Tchamo <francktchamo@yahoo.fr>
2022-04-21 21:22:20 -0700

    Add free_list function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: a pointer to the struct list_t
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
