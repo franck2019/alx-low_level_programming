db8fddb1fc4cae4f30ceac39372271b398ada0c3
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-03 05:31:10 -0700

    Add read_textfile function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: the name of the file to read
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *str;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	str = malloc(sizeof(char) * (letters + 1));
+
+	if (str == NULL)
+		return (0);
+	/* read the text file */
+	r = read(fd, str, letters);
+	if (r == -1)
+		return (0);
+	str[letters] = '\0';
+	w = write(STDOUT_FILENO, str, r);
+
+	if (w == -1)
+		return (0);
+
+	close(fd);
+	free(str);
+	return (w);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>                                                                        
+#include <stdlib.h>                                                                       
+#include <string.h>                                                                       
+                                                                                          
+#include <sys/types.h>                                                                    
+#include <sys/stat.h>                                                                     
+#include <fcntl.h>                                                                        
+                                                                                          
+#include <unistd.h>  
+
+/* _putchar prototype */
+int _putchar(char c);
+/* read_textfile prototype */
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* MAIN_H */
