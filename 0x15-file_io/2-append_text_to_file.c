61f0c06a721d6d7ceb0046fe143debce476a34b7
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-03 08:26:17 -0700

    Add ppend_text_to_file function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-		w = write(fd, text_content, strlen(text_content) + 1);
+		w = write(fd, text_content, strlen(text_content));
 
 	if (w == -1)
 		return (-1);
