8d3000c8a18a090b0f4516aea01a4562a4850cd9
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-06 09:08:43 -0700

    Update insert_nodeint function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -57,6 +57,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			left = temp;
 			temp = temp->next;
+			if (temp == NULL)
+				return (NULL);
 			right = temp;
 			counter++;
 		}
