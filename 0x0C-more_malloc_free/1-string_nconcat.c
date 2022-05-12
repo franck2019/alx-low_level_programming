b753eb13652c04afde628e7141c8b4432020ea69
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-11 18:24:32 -0700

    Update string_nconcat function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,32 +14,23 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *tmp;
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i = 0;
+	unsigned int len1, len2, i = 0;
 
 	len1 = s1 == NULL ? 1 : strlen(s1);
 	len2 = s2 == NULL ? 1 : strlen(s2);
 	if (n >= len2)
 		n = len2;
-	/* we'll add 1 for '\0' of s1 and 1 for '\0' of s2 */
 	tmp = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (tmp == NULL)
-	{
 		return (NULL);
-	}
-	
+
 	if (s1 == NULL)
 		tmp[0] = '\0';
 	else
-		/* copy s1 into tmp from 0 to len-1 */
 		for (i = 0; s1[i] != '\0'; i++)
-		{
 			tmp[i] = s1[i];
-		}
-			
-	/* copy s2 into tmp from len+2 position */
+
 	if (s2 == NULL)
 	{
 		tmp[1] = '\0';
@@ -48,9 +39,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	for (i = 0; i < n && s2[i] != '\0'; i++)
-	{
 		tmp[len1 + i] = s2[i];
-	}
+
 	tmp[len1 + n] = '\0';
 
 	return (tmp);
