e8cd51881a5818c2f941186f1a1575c7dcc8336c
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-12 17:16:50 -0700

    Add strtow function

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - function that splits a string into words.
+ * @str: a string
+ *
+ * Return: a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	char *res;
+
+	return (&res);
+}
