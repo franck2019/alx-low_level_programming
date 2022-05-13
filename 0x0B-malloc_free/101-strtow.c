054f4c1d232bb5ab48624b8aeb1983816ab9a34b
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-12 17:32:52 -0700

    Update strtow function

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,8 +12,30 @@
 char **strtow(char *str)
 {
 	char **res;
+	int i, word_counter, keeper;
 
-	if (str == NULL || str = "")
+	if (str == NULL || str == "")
+		return (NULL);
+
+	word_counter = 0;
+	keeper = 0;
+
+	for (i = 0; str[i] = '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			keeper = 0;
+		}
+		else if (keeper == 0)
+		{
+			keeper = 1;
+			word_counter++;
+		}
+	}
+
+	res = (char **) malloc(sizeof(char) * (word_counter + 1));
+
+	if (res == NULL)
 		return (NULL);
 
 	return (res);
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -17,4 +17,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 /* argstostr prototype */
 char *argstostr(int ac, char **av);
+/* strtow prototype */
+char **strtow(char *str);
 #endif /* HOLBERTON_H */
