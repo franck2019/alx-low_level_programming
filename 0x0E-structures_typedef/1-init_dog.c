301ba821ddeca29731e43624fd1ec5c0457056f4
Franck Tchamo <francktchamo@yahoo.fr>
2022-05-20 08:42:23 -0700

    Update init_dog function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *tmp = d;
-
-	if(tmp == NULL)
-		exit(-1);
-	tmp->name = name;
-	tmp->age = age;
-	tmp->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
