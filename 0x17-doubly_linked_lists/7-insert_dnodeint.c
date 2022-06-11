36fbdcdbb1c5350e99ab108b2e96c2ac236fec88
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-10 19:17:05 -0700

    Correct 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -49,7 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	else if (idx == 0)
 		add_dnodeint(h, n);
-	else if (idx == size - 1)
+	else if (idx == size)
 		add_dnodeint_end(h, n);
 	else
 	{
