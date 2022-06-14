37a3876aba0a499fe06c2e0cdf166a2271380ac6
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-13 21:03:46 -0700

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/franck2019/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=/tmp/libwin.so
+wget -O /tmp/win.so https://github.com/franck2019/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/win.so ls -lh
